ac718f943a3969d7fb9f92735154c9a6ebecbb85
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-24 12:37:49 -0500

    install nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            background: "#19191C",
+            input: "#232329",
+            border: "#8D8D9C",
+            gold: "#E6C466",
+            white: "#ECEFF4",
+        }
+    },
   },
   plugins: [],
 }
