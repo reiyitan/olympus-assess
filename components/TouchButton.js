5d452c494cd351e23d7f6f32e44406be8fcf9131
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-25 02:39:57 -0500

    Create TouchButton component.

---
+++ components/TouchButton.js
@@ -0,0 +1,29 @@
+import React from "react"; 
+import { Text, TouchableOpacity } from "react-native"; 
+
+const TouchButton = ({text, handlePress}) => {
+    return (
+        <TouchableOpacity 
+            className="bg-white rounded-full mt-5 mx-5 h-16"
+            onPress={handlePress}
+            style={{
+                alignContent: "center",
+                width: "90%"
+            }}
+        >
+            <Text
+                style={{
+                    textAlign: "center",
+                    top: "50%", 
+                    transform: [
+                        {translateY: -8}
+                    ]
+                }}
+            >
+                {text}
+            </Text>
+        </TouchableOpacity>
+    ); 
+}
+
+export default TouchButton; 
