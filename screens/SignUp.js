99c852ab1b6c963c9f0ca3003d50b578e841e867
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-25 02:40:24 -0500

    Create Forms for SignUp page.

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -3,13 +3,13 @@ import { useState } from "react";
 import { View } from "react-native";
 import Form from "../components/Form";
 
-const SignUp = () => {
+const SignUp = ({navigation}) => {
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState(""); 
     const [confirmPass, setConfirmPass] = useState(""); 
 
     return (
-        <View className="h-full bg-background color-white">
+        <View className="color-white">
             <Form
                 text={email}
                 setText={setEmail}
