773789971b1e5e0e01159a7e6ef9e0b896d32c3d
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-25 03:58:04 -0500

    Setup login screen.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,40 @@
 import React from "react"; 
-import { View, Text } from "react-native"; 
-import { Form } from "../components";
+import { useState } from "react"; 
+import { View } from "react-native"; 
+import { Form, TouchButton } from "../components";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({navigation}) => {
     const [email, setEmail] = useState(""); 
     const [pass, setPass] = useState(""); 
-    const [confirmPass, setConfirmPass] = useState(""); 
+    const handleLogin = () => {
+        //navigate to home screen
+    }
 
     return (
-        <View>
+        <View
+            style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+        >
             <Form 
-
+                text={email}
+                setText={setEmail}
+                secure={false}
+                placeholder="Email"
+            />
+            <Form 
+                text={pass}
+                setText={setPass}
+                secure={true}
+                placeholder="Password"
+            />
+            <TouchButton 
+                text="Login"
+                handlePress={handleLogin}
             />
         </View>
     );
