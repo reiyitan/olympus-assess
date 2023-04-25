41a09071552260935649c9a653e0b4961f725393
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-25 03:52:58 -0500

    Add documentation. Add new prop: secure. If true, hides user input.

--- components/Form.js
+++ components/Form.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { TextInput } from "react-native";
 
-export const Form = ({text, setText, placeholder}) => {
-
+/**
+ * Component for getting user input. 
+ * 
+ * @param text - A state variable. Replaces the value of the TextInput. 
+ * @param setText - Updates state of text when a user types in the TextInput. 
+ * @param secure - True if text should be hidden, false otherwise. 
+ * @param placeholder - Placeholder text for the form. 
+ */
+export const Form = ({text, setText, secure, placeholder}) => {
     return (
         <TextInput 
-            className="h-16 bg-input border-border mx-5 mt-5 rounded-full color-gold"
+            className="h-16 bg-input border-border mt-5 rounded-full color-gold"
+            secureTextEntry={secure}
             value={text}
             placeholder={placeholder}
             placeholderTextColor="#4C566A"
             onChangeText={setText}
-            textAlign="center"
+            textAlign="left"
+            style={{
+                paddingLeft: 30,
+                width: "95%"
+            }}
         />
     );
 }
