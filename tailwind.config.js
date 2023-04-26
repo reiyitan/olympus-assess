7d188082ebfe9c5b50ef8c8deb655d591848f374
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-25 19:23:03 -0500

    remove unused color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
             input: "#232329",
             border: "#8D8D9C",
             gold: "#E6C466",
-            white: "#ECEFF4",
             red: "#FF6565"
         }
     },
