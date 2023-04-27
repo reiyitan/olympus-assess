f6fad06dc5f912d5751f76b869a25e0a76a7e261
Rei-Yi Tan <tanreiyi@gmail.com>
2023-04-27 01:31:21 -0500

    add 2 more ExerciseRows

--- components/Exercise.js
+++ components/Exercise.js
@@ -17,6 +17,8 @@ export const Exercise = () => {
             />
             <Header />
             <ExerciseRow />
+            <ExerciseRow />
+            <ExerciseRow />
         </>
     );
 }
